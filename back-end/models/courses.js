08ba8147778404648900bc9cd4f57cddb38d7e14
apramodya <pramodyaabeysinghe@gmail.com>
2018-05-28 12:01:39 +0530

    Create course module and routes
    Create course, get course details by coures code, semester and year

--- back-end/models/courses.js
+++ back-end/models/courses.js
@@ -25,3 +25,15 @@ const Course = module.exports = mongoose.model('Course', CourseSchema);
 module.exports.addCourse = function (course, callback) {
     course.save(callback);
 };
+
+module.exports.getCourseByCourseCode = function (course_code, callback) {
+    Course.findOne({ 'course_code' : course_code}, callback);
+};
+
+module.exports.getCoursesByYear = function (year, callback) {
+    Course.find({ 'year' : year}, callback);
+};
+
+module.exports.getCoursesBySemester = function (semester, callback) {
+    Course.find({ 'semester' : semester}, callback);
+};
--- back-end/routes/courses.js
+++ back-end/routes/courses.js
@@ -16,4 +16,40 @@ router.post('/add', function (req, res, next) {
     })
 });
 
+// get course by course code
+router.get('/getCourse/:code', function (req, res, next) {
+    let course_code = req.params.code;
+    Course.getCourseByCourseCode(course_code, function (err, course) {
+        if (err) return res.json({success: false, msg: 'Failed to find course'});
+        else return res.json({
+            success: true,
+            msg: 'Found course',
+            course: {
+                course_name: course.course_name,
+                course_code: course.course_code,
+                year: course.year,
+                semester: course.semester
+            }
+        });
+    })
+});
+
+// get courses by year
+router.get('/getCoursesByYear/:year', function (req, res, next) {
+    let year = req.params.year;
+    Course.getCoursesByYear(year, function (err, courses) {
+        if (err) return res.json({success: false, msg: 'Failed to find courses'});
+        else return res.json(courses);
+    })
+});
+
+// get courses by semester
+router.get('/getCoursesBySemester/:semester', function (req, res, next) {
+    let semester = req.params.semester;
+    Course.getCoursesBySemester(semester, function (err, courses) {
+        if (err) return res.json({success: false, msg: 'Failed to find courses'});
+        else return res.json(courses);
+    })
+});
+
 module.exports = router;
