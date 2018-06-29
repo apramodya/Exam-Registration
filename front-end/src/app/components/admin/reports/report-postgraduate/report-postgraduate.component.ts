8be2767de24bc2dfed45796cebe4c7d4adf53fa0
apramodya <pramodyaabeysinghe@gmail.com>
2018-06-30 00:28:40 +0530

    Update JS pdf to Js pdf-autotable

--- front-end/src/app/components/admin/reports/report-postgraduate/report-postgraduate.component.ts
+++ front-end/src/app/components/admin/reports/report-postgraduate/report-postgraduate.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as JsPDF from 'jspdf';
+import 'jspdf-autotable';
 
 @Component({
   selector: 'app-report-postgraduate',
@@ -10,6 +12,21 @@ export class ReportPostgraduateComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  
+
+  genPDF() {
+    var columns = ["ID", "Name", "Country"];
+    var rows = [
+      [1, "Shaw", "Tanzania"],
+      [2, "Nelson", "Kazakhstan"],
+      [3, "Garcia", "Madagascar"]
+    ];
+
+    let doc = new JsPDF('p', 'pt');
+    doc.autoTable(columns, rows);
+    doc.save("table.pdf");
   }
 
 }
